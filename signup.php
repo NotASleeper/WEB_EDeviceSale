7f766df9d9dcba426d988bfe2191b0a49893c6b4
22520570 <22520570@gm.uit.edu.vn>
2025-01-10 10:12:36 +0700

    check user age when sign up

--- signup.php
+++ signup.php
@@ -29,43 +29,52 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $cfpass_customer = $_POST['cf-password'];     //password
     $cfpass_customer = filter_var($cfpass_customer, FILTER_SANITIZE_STRING);
 
-    if ($pass_customer === $cfpass_customer) {
-        //check if already exists
-        $select_cus = $conn->prepare("SELECT * FROM `customer` WHERE phone_no = ? OR username = ?");
-        $select_cus->execute([$phone_no, $username_customer]);
-        $row = $select_cus->fetch(PDO::FETCH_ASSOC);
-        //if existed
-        if ($select_cus->rowCount() > 0) {
-            $message[] = "Phone or username has already existed!";
-        } else {
-            //check emp
-            $select_emp = $conn->prepare("SELECT * FROM `employee` WHERE phone_to = ? OR username = ?");
-            $select_emp->execute([$phone_no, $username_customer]);
-            $row = $select_emp->fetch(PDO::FETCH_ASSOC);
-            if ($select_emp->rowCount() > 0) {
+    // Kiểm tra tuổi
+    $dob = new DateTime($date_of_birth);
+    $today = new DateTime();
+    $age = $today->diff($dob)->y;
+
+    if ($age < 18) {
+        $message[] = "Customer should be over 18 years old";
+    } else {
+        if ($pass_customer === $cfpass_customer) {
+            //check if already exists
+            $select_cus = $conn->prepare("SELECT * FROM `customer` WHERE phone_no = ? OR username = ?");
+            $select_cus->execute([$phone_no, $username_customer]);
+            $row = $select_cus->fetch(PDO::FETCH_ASSOC);
+            //if existed
+            if ($select_cus->rowCount() > 0) {
                 $message[] = "Phone or username has already existed!";
             } else {
-                //if new, not exists
-
-                //hash password
-                $hashed_password = password_hash($pass_customer, PASSWORD_DEFAULT);
-
-                //insert to db
-                $insert_cus = $conn->prepare("INSERT INTO `customer` (name_customer, date_of_birth, phone_no, total_spending, username, password) VALUES (?,?,?,?,?,?)");
-                $insert_cus->execute([$name_customer, $date_of_birth, $phone_no, 0, $username_customer, $hashed_password]);
-
-                //check
-                $confirm_cus = $conn->prepare("SELECT * FROM `customer` WHERE phone_no = ?");
-                $confirm_cus->execute([$phone_no]);
-                if ($confirm_cus->rowCount() > 0) {
-                    header('location:login.php');
+                //check emp
+                $select_emp = $conn->prepare("SELECT * FROM `employee` WHERE phone_to = ? OR username = ?");
+                $select_emp->execute([$phone_no, $username_customer]);
+                $row = $select_emp->fetch(PDO::FETCH_ASSOC);
+                if ($select_emp->rowCount() > 0) {
+                    $message[] = "Phone or username has already existed!";
                 } else {
-                    echo '<script>alert("Failed to insert");</script>';
+                    //if new, not exists
+
+                    //hash password
+                    $hashed_password = password_hash($pass_customer, PASSWORD_DEFAULT);
+
+                    //insert to db
+                    $insert_cus = $conn->prepare("INSERT INTO `customer` (name_customer, date_of_birth, phone_no, total_spending, username, password) VALUES (?,?,?,?,?,?)");
+                    $insert_cus->execute([$name_customer, $date_of_birth, $phone_no, 0, $username_customer, $hashed_password]);
+
+                    //check
+                    $confirm_cus = $conn->prepare("SELECT * FROM `customer` WHERE phone_no = ?");
+                    $confirm_cus->execute([$phone_no]);
+                    if ($confirm_cus->rowCount() > 0) {
+                        header('location:login.php');
+                    } else {
+                        echo '<script>alert("Failed to insert");</script>';
+                    }
                 }
             }
+        } else {
+            echo '<script>alert("Password and Confirm password not matched");</script>';
         }
-    } else {
-        echo '<script>alert("Password and Confirm password not matched");</script>';
     }
 
 
